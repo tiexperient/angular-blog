efd2b726fb84204dd2eb487b0654aa3fdd95a837
Celia <celia.megatronn@gmail.com>
2025-01-12 23:33:56 -0300

    create article

--- src/app/dados/dataFake.ts
+++ src/app/dados/dataFake.ts
@@ -4,32 +4,53 @@ export const dataFake = [
         "title": "Rotina de estudos se faz fundamental",
         "time":"Janeiro/2025",
         "description": `
-        Nos dias atuais, o mercado de tecnologia tem se tornado cada vez mais competitivo. Como aspirante a Front-end Developer especializada em Angular, percebo que a chave para me destacar nesse ambiente desafiador é uma rotina de estudos bem planejada e consistente. Este artigo é um relato pessoal da minha jornada de aprendizado e como a disciplina tem sido essencial para meu progresso.
+        Nos dias atuais, o mercado de tecnologia tem se tornado cada vez mais competitivo. Como aspirante a Front-end Developer 
+        especializada em Angular, percebo que a chave para me destacar nesse ambiente desafiador é uma rotina de estudos bem 
+        planejada e consistente. Este artigo é um relato pessoal da minha jornada de aprendizado e como a disciplina tem sido 
+        essencial para meu progresso.
         <br><br>
         <h2>O ponto de partida</h2>
-        Quando decidi me aprofundar no Angular, sabia que não seria uma tarefa fácil. Este é um framework robusto e muito utilizado no desenvolvimento de aplicações web. Para acompanhar a evolução do mercado e atender às demandas das empresas, compreendi que precisaria dedicar tempo e esforço.
+        Quando decidi me aprofundar no Angular, sabia que não seria uma tarefa fácil. Este é um framework robusto e muito utilizado 
+        no desenvolvimento de aplicações web. Para acompanhar a evolução do mercado e atender às demandas das empresas, compreendi 
+        que precisaria dedicar tempo e esforço.
         <br>
-        Meu primeiro passo foi organizar minha rotina. Criei um cronograma que incluiísse estudos teóricos, prática de código e revisão dos conceitos aprendidos. Dividi meu tempo entre leitura de documentação oficial, acompanhamento de cursos online e participação em comunidades de desenvolvedores.
+        Meu primeiro passo foi organizar minha rotina. Criei um cronograma que incluísse estudos teóricos, prática de código e 
+        revisão dos conceitos aprendidos. Dividi meu tempo entre leitura de documentação oficial, acompanhamento de cursos online 
+        e participação em comunidades de desenvolvedores.
         <br><br>
         <h2>Disciplina como base do aprendizado</h2>
-        Manter uma rotina consistente exige disciplina. Reservei horários fixos diários para os estudos e tratei esses momentos como compromissos inadiáveis. Essa regularidade foi fundamental para manter meu foco e garantir que o aprendizado não fosse interrompido.
+        Manter uma rotina consistente exige disciplina. Reservei horários fixos diários para os estudos e trato esses momentos 
+        como compromissos inadiáveis. Essa regularidade tem sido fundamental para manter meu foco e garantir que o aprendizado não 
+        seja interrompido.
         <br>
-        Também adotei métodos que maximizam minha produtividade, como o técnica Pomodoro, que alterna períodos de estudo intenso com pausas curtas. Além disso, estabeleci metas semanais, como concluir um módulo de curso ou desenvolver um pequeno projeto, o que me ajuda a visualizar meu progresso e me motiva a continuar.
+        Também adotei métodos que maximizam minha produtividade, estabelecendo metas semanais, como concluir um módulo de curso 
+        ou desenvolver um pequeno projeto, o que me ajuda a visualizar meu progresso e me motiva a continuar.
         <br><br>
         <h2>Explorando o Angular na prática</h2>
-        Para solidificar meu conhecimento, passei a construir aplicações reais com Angular. Essa abordagem prática me permitiu entender como o framework funciona em cenários do mundo real. Criei projetos pessoais, como um gerenciador de tarefas e um sistema de controle de finanas, aplicando conceitos como rotas, serviços, injeção de dependência e comunicação entre componentes.
+        Para solidificar meu conhecimento, passei a construir aplicações reais com Angular. Essa abordagem prática me permite 
+        entender como o framework funciona em cenários do mundo real. Criei projetos pessoais, como esse blog maravilhoso e 
+        um sistema de marketplace dos meus Doramas favoritos, que em breve será publicado, aplicando conceitos como rotas, 
+        serviços, injeção de dependência e comunicação entre componentes.
         <br>
-        Além disso, participei de desafios de código e revisei aplicações open source, o que me deu uma visão mais ampla sobre soluções criativas e boas práticas de desenvolvimento.
+        Além disso, participei de desafios de código em bootcamps e revisei aplicações com Angular Material, Bootstrap, criação de
+        APIs para conexão com Banco de Dados e tratamento de responsividade, o que me deu uma visão mais ampla sobre 
+        soluções criativas e boas práticas de desenvolvimento.
         <br><br>
         <h2>Motivando-se em meio à jornada</h2>
-        Manter a motivação é essencial para continuar evoluindo. Busco inspiração em histórias de outros desenvolvedores, celebro pequenas conquistas e relembro constantemente meu objetivo maior: conquistar uma vaga como Front-end Developer Angular.
+        Manter a motivação é essencial para continuar evoluindo. Busco inspiração em histórias de outros desenvolvedores, 
+        celebro pequenas conquistas e relembro constantemente meu objetivo maior: conquistar uma vaga como Front-end Developer 
+        Angular.
         <br>
-        Ademais, interagir com a comunidade Angular tem sido enriquecedor. A troca de experiências, o suporte de outros desenvolvedores e a possibilidade de colaborar em projetos me dão uma perspectiva valiosa sobre o mercado.
+        Ademais, interagir com a comunidade Angular tem sido enriquecedor. A troca de experiências, o suporte de outros 
+        desenvolvedores e a possibilidade de colaborar em projetos me dão uma perspectiva valiosa sobre o mercado.
         <br><br>
         <h2>Conclusão</h2>
-        A rotina de estudos tem sido um alicerce para meu crescimento como desenvolvedora. Com planejamento, disciplina e dedicação, acredito que estou trilhando um caminho sólido em direção ao meu objetivo profissional. Minha experiência com o Angular não é apenas uma jornada de aprendizado, mas também um processo de autodescoberta e superação.
+        A rotina de estudos tem sido um alicerce para meu crescimento como desenvolvedora. Com planejamento, disciplina e 
+        dedicação, acredito que estou trilhando um caminho sólido em direção ao meu objetivo profissional. Minha experiência 
+        com o Angular não é apenas uma jornada de aprendizado, mas também um processo de autodescoberta e superação.
         <br>
-        Se você também está em busca de se destacar no mercado de tecnologia, convido-o a refletir sobre sua rotina e a investir no seu crescimento. Afinal, a disciplina é o que transforma sonhos em conquistas reais.
+        Se você também está em busca de se destacar no mercado de tecnologia, convido-lhe a refletir sobre sua rotina e a investir 
+        no seu crescimento. Afinal, a disciplina é o que transforma sonhos em conquistas reais.
         `,
         "photoCover": "assets/img/rotina-estudo.jpeg"
     },
