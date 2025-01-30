2686cb463cf61fb5a518df146e0c752fc6832883
Celia <celia.megatronn@gmail.com>
2025-01-30 14:46:01 -0300

    previous projects

--- src/app/components/latest-blog-title/latest-blog-title.component.ts
+++ src/app/components/latest-blog-title/latest-blog-title.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-latest-blog-title',
   templateUrl: './latest-blog-title.component.html',
-  styleUrls: ['./latest-blog-title.component.css', './latest-blog-title.responsive.css']
+  styleUrls: ['./latest-blog-title.component.css', './latest-blog-title.component.responsive.css']
 })
 export class LatestBlogTitleComponent implements OnInit {
 
